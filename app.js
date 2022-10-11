56bce52f81400802352cd378fb9f2ed9aeee537c
0shuvo0 <seriouslolz1@gmail.com>
2022-10-11 10:03:19 +0600

    game start functionality1~

--- app.js
+++ app.js
@@ -17,6 +17,38 @@ const generateRoomId = () => (+ new Date()) + '-' + parseInt(Math.random() * 999
 
 app.use(cors())
 
+function createGameStartData(){
+  const gameData = {
+    ballData: {
+      x: 50,
+      y: 50,
+      vx: Math.random() + 2,
+      vy: Math.random() + 2
+    },
+    leftPadData: {
+        y: 30
+    },
+    rightPadData: {
+        y: 60
+    }
+  }
+  gameData.ballData.x = 50 + (Math.random() * 50)
+  gameData.ballData.y = 50 + (Math.random() * 50)
+  if(Math.random() > .5) gameData.ballData.vx *= -1
+  if(Math.random() > .5) gameData.ballData.vy *= -1
+
+  return gameData
+}
+
+function checkReadyState(roomId, opponentSocket, socket){
+  const room = rooms[roomId]
+  const playersReady = room.userReady && room.opponentReady
+  if(!playersReady) return
+  const startData = createGameStartData()
+  opponentSocket.emit('startGame', startData)
+  socket.emit('startGame', startData)
+}
+
 io.on('connection', socket => {
   socket.on('createRoom', data => {
     const roomId = generateRoomId()
@@ -29,6 +61,19 @@ io.on('connection', socket => {
       roomId,
       userType: 'challanged'
     })
+  })
+
+  socket.on('playerReady', roomId => {
+    rooms[roomId].userReady = true
+    const room = rooms[roomId]
+    io.to(room.opponentId).emit('opponentReady')
+    checkReadyState(roomId, io.to(room.opponentId), socket)
+  })
+  socket.on('opponentReady', roomId => {
+    rooms[roomId].opponentReady = true
+    const room = rooms[roomId]
+    io.to(room.userId).emit('opponentReady')
+    checkReadyState(roomId, io.to(room.opponentId), socket)
   })
 })
 
