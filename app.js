8a9c05d509a0756fb6513c33a396c3df949fc617
0shuvo0 <seriouslolz1@gmail.com>
2022-10-12 10:09:16 +0600

    game over

--- app.js
+++ app.js
@@ -17,23 +17,23 @@ const generateRoomId = () => (+ new Date()) + '-' + parseInt(Math.random() * 999
 
 app.use(cors())
 
+const TOTAL_ROUND = 5
+
 function createGameStartData(){
   const gameData = {
     ballData: {
-      x: 50,
-      y: 50,
-      vx: Math.random() + 2,
-      vy: Math.random() + 2
+      x:  50 + (Math.random() * 50),
+      y: 50 + (Math.random() * 50),
+      vx: Math.max(.6, Math.random() + .2),
+      vy: Math.max(.6, Math.random() + .2)
     },
     leftPadData: {
-        y: 30
+        y: 40
     },
     rightPadData: {
-        y: 60
+        y: 40
     }
   }
-  gameData.ballData.x = 50 + (Math.random() * 50)
-  gameData.ballData.y = 50 + (Math.random() * 50)
   if(Math.random() > .5) gameData.ballData.vx *= -1
   if(Math.random() > .5) gameData.ballData.vy *= -1
 
@@ -52,7 +52,13 @@ function checkReadyState(roomId, opponentSocket, socket){
 io.on('connection', socket => {
   socket.on('createRoom', data => {
     const roomId = generateRoomId()
-    rooms[roomId] = data
+    rooms[roomId] = {
+      ...data,
+      score: {
+        user: 0,
+        opponent: 0
+      }
+    }
     socket.emit('roomCreated', {
       roomId,
       userType: 'challanger'
@@ -74,6 +80,28 @@ io.on('connection', socket => {
     const room = rooms[roomId]
     io.to(room.userId).emit('opponentReady')
     checkReadyState(roomId, io.to(room.opponentId), socket)
+  })
+
+  socket.on('playerPadMove', data => {
+    const room = rooms[data.roomId]
+    io.to(room[data.player === 'opponent' ? 'userId' : 'opponentId']).emit('opponentPadMove', data.direction)
+  })
+
+  socket.on('scoreUpdate', data => {
+    rooms[data.roomId].score = data.score
+    const room = rooms[data.roomId]
+    
+    if(room.score.user + room.score.opponent >= TOTAL_ROUND){
+      const data = {
+        score: room.score,
+        winner: room.score.user === room.score.opponent ? 'tie' : (room.score.user > room.score.opponent ? 'player' : 'opponent')
+      }
+      socket.emit('gameOver', data)
+      io.to(room.opponentId).emit('gameOver', data)
+      return
+    }
+
+    io.to(room.opponentId).emit('scoreUpdate', data.score)
   })
 })
 
